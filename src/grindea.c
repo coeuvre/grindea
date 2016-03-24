f58c63cbd9d25f42be88782c8a05f491c0858931
Coeuvre Wong <coeuvre@gmail.com>
2016-03-24 00:13:52 +0000

    Update

--- src/grindea.c
+++ src/grindea.c
@@ -36,21 +36,12 @@ init_game_state(HM_GameMemory *memory, GameState *gamestate) {
     gamestate->hero_pos = hm_v2(WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f);
 }
 
-HM_CONFIG {
-    config->window.title = "Grindea";
-    config->window.width = WINDOW_WIDTH;
-    config->window.height = WINDOW_HEIGHT;
-    config->perm_memory_size = HM_MB(64);
-    config->tran_memory_size = HM_MB(128);
-    config->is_exit_on_esc = true;
-}
-
-HM_INIT_GAME {
+static HM_INIT_GAME(init_game) {
     GameState *gamestate = HM_PUSH_STRUCT(&memory->perm, GameState);
     init_game_state(memory, gamestate);
 }
 
-HM_UPDATE_AND_RENDER {
+static HM_UPDATE_AND_RENDER(update_and_render) {
     f32 dt = input->dt;
 
     GameState *gamestate = memory->perm.base;
@@ -63,17 +54,44 @@ HM_UPDATE_AND_RENDER {
 
     hm_clear_texture(framebuffer, hm_v4(0.5f, 0.5f, 0.5f, 0));
 
+    /*for (int i = 0; i < 10; ++i)*/
     {
-        HM_V2 hero_pos_in_camera_space = hm_v2_sub(gamestate->hero_pos, gamestate->camera_pos);
+        /*HM_V2 hero_pos_in_camera_space = hm_v2_sub(gamestate->hero_pos, gamestate->camera_pos);*/
 
         HM_Transform2 transform = hm_transform2_identity();
-        /*transform = hm_transform2_rotate_by(transform, HM_DEG_TO_RAD(60.0));*/
-        /*transform = hm_transform2_scale_by(transform, 2, 1);*/
-        /*transform = hm_transform2_translate_by(transform, input->mouse.x, WINDOW_HEIGHT - input->mouse.y);*/
-        transform = hm_transform2_translate_by(transform, hero_pos_in_camera_space);
+        transform = hm_transform2_rotate_by(transform, gamestate->time);
+        transform = hm_transform2_scale_by(transform, hm_v2(2, 2));
+        /*transform = hm_transform2_translate_by(transform, hm_v2(input->mouse.x, WINDOW_HEIGHT - input->mouse.y));*/
+        /*transform = hm_transform2_translate_by(transform, hero_pos_in_camera_space);*/
 
         transform = hm_transform2_translate_by(transform, screen_center);
         HM_Sprite *frame = gamestate->hero_walk_down_anim + ((u32)(15 * gamestate->time) % HM_ARRAY_COUNT(gamestate->hero_walk_down_anim));
+        /*frame = gamestate->hero_walk_down_anim;*/
         hm_draw_sprite(framebuffer, transform, frame, hm_v4(1, 1, 1, 1));
+        /*hm_draw_bitmap(framebuffer, transform, &gamestate->hero_walk_down_texture, hm_v4(1, 1, 1, 1));*/
+
     }
+
+    {
+        HM_Transform2 transform = hm_transform2_identity();
+        transform = hm_transform2_translate_by(transform, hm_v2(-50, -50));
+        transform = hm_transform2_rotate_by(transform, gamestate->time);
+        transform = hm_transform2_translate_by(transform, hm_v2(300, 300));
+
+        hm_draw_bbox2(framebuffer, transform, hm_bbox2(hm_v2(0, 0), hm_v2(100, 100)), hm_v4(1, 0, 0, 1));
+    }
+}
+
+int main(void) {
+    HM_Config config = {};
+    config.window.title = "Grindea";
+    config.window.width = WINDOW_WIDTH;
+    config.window.height = WINDOW_HEIGHT;
+    config.memory.size.perm = HM_MB(64);
+    config.memory.size.tran = HM_MB(128);
+    config.debug.is_exit_on_esc = true;
+    config.callback.init_game = init_game;
+    config.callback.update_and_render = update_and_render;
+
+    hm_run(&config);
 }
